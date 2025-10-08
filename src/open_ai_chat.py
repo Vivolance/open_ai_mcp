55ff7c5860b7616dc896aaf57c32da2748911fbe
Elson <elsonccj@gmail.com>
2025-10-08 09:50:20 +0800

    black ruff

--- src/open_ai_chat.py
+++ src/open_ai_chat.py
@@ -8,19 +8,20 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-system_prompt: str = "You are an expert in Finance specifically in Quantitative Investment and Trading"
+system_prompt: str = (
+    "You are an expert in Finance specifically in Quantitative Investment and Trading"
+)
 
 user_prompt: str = input("What is your question?")
 
 chat_completion = client.chat.completions.create(
     messages=[
         {"role": "system", "content": system_prompt},
-        {"role": "user", "content": user_prompt}
+        {"role": "user", "content": user_prompt},
     ],
-    model="gpt-4o"
+    model="gpt-4o",
 )
 
 response_text: str = chat_completion.choices[0].message.content
 
 print(response_text)
-
